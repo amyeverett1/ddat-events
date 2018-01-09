40b50776000ac4692805ac561921f7128c23e2b8
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-01-09 08:48:26 +0000

    add in new content for roundtables

--- index.php
+++ index.php
@@ -26,19 +26,25 @@
   </div><!-- end agenda-->
     
     
-    <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Roundtables - Coming Soon</span></h3>
+    <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Roundtables</span></h3>
   <div>
 
          <div class="wrapper">
             <div class="box white">
           
    
-   <p>Roundtable dates for 2018 will be added soon.</p>
+   <p>The Digital, Data and Technology roundtables will take place across our main locations on a quarterly basis. These are the proposed DDaT Roundtable dates for 2018 so far:</p>
+    <ul>
+        <li>Croydon - 25 January</li>
+        <li>Hendon - 7 February</li>
+        <li>Bedfont Lakes - 27 February</li>
+    </ul>           
+</p>
 
                 
-                <!--   <div class="button_container_first">
+        <div class="button_container_first">
                 <a class="btTxt" href="http://ddat-together-roundtable.herokuapp.com">View roundtable events</a>
-            </div> -->
+             </div>
                 
 
            </div><!-- end boxwhite-->
