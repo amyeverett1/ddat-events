b71ad259fa7275d3ee46b8fadd59b533c079099d
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-01-09 08:42:42 +0000

    add in all files

---
+++ footer.php
@@ -0,0 +1,18 @@
+        <footer>
+            <p>For the best experience using this site, we recommend you use Google Chrome as your browser.</p>
+        </footer>
+
+    <script src="lib/jquery-2.0.3.js"></script>
+<script src="src/js/jquery.swipebox.js"></script>
+
+<script type="text/javascript">
+;( function( $ ) {
+
+	$( '.swipebox' ).swipebox();
+
+} )( jQuery );
+</script>
+
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php include('header.php') ?>
+    <h1>DDaT Together Events</h1>
+</header>
+
+<div class="date">
+    <span>Welcome to the Digital, Data and Technology event booking app, your one stop shop for key DDaT Together events</span>
+</div>
+
+<div class="container index"> 
+<div class="accordion">    
+
+      
+<h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>DDaT Together - 20 February 2018</span></h3> 
+<div>
+<div class="wrapper">
+   
+    <p>The next DDaT Together event is scheduled for 20 February 2018 at the Kia Oval. Further details will be added in due course.</p>
+    
+   <!--    <div class="button_container_first">
+                <a class="btTxt" href="http://ddat-together.herokuapp.com/">View event</a>
+            </div> --> 
+    
+
+    </div> <!-- wrapper-->
+    
+  </div><!-- end agenda-->
+    
+    
+    <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Roundtables - Coming Soon</span></h3>
+  <div>
+
+         <div class="wrapper">
+            <div class="box white">
+          
+   
+   <p>Roundtable dates for 2018 will be added soon.</p>
+
+                
+                <!--   <div class="button_container_first">
+                <a class="btTxt" href="http://ddat-together-roundtable.herokuapp.com">View roundtable events</a>
+            </div> -->
+                
+
+           </div><!-- end boxwhite-->
+        </div> <!-- wrapper-->    
+      
+    </div>
+    
+  <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT All-Staff WebEx events - Coming Soon</span></h3>
+  <div>
+
+         <div class="wrapper">
+            <div class="box white">
+          
+   <p>Details of the next All-Staff WebEx will be added in 2018.</p>
+             
+   
+                  <!-- <div class="button_container_first">
+                <a class="btTxt" href="https://www.eventbrite.co.uk/e/ddat-all-staff-dial-in-tickets-40187871004">Book a ticket</a>
+            </div> -->
+                
+
+           </div><!-- boxwhite-->
+        </div> <!-- wrapper-->    
+      
+      
+      
+    </div>
+    
+    
+    <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Community Events - Coming Soon</span></h3>
+  <div>
+
+         <div class="wrapper">
+            <div class="box white">
+          
+   <p>Details coming soon.</p>
+             
+   
+                  <!-- <div class="button_container_first">
+                <a class="btTxt" href="https://www.eventbrite.co.uk/e/ddat-all-staff-dial-in-tickets-40187871004">Book a ticket</a>
+            </div> -->
+                
+
+           </div><!-- boxwhite-->
+        </div> <!-- wrapper-->    
+      
+      
+      
+    </div>
+    
+    
+    
+            
+     </div> <!--end CONTAINER--> 
+
+  
+</div> <!--end ACCORDION--> 
+    
+          
+
+<?php include('footer.php')?>
