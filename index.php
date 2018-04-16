5fb5927fc9563acc98ee2084de25589606a555c1
amyeverett1 <amy.everett@digital.homeoffice.gov.uk>
2018-04-16 19:35:24 +0100

    add logo

--- index.php
+++ index.php
@@ -1,104 +1,104 @@
 <?php include('header.php') ?>
-    <h1>DDaT Together Events</h1>
+<div class="logo_box">  <img src="/images/logo.png" alt="#"/> </div> <h1>DDaT Together Events</h1>
 </header>
 
 <div class="date">
     <span>Welcome to the Digital, Data and Technology event booking app, your one stop shop for key DDaT Together events</span>
 </div>
 
-<div class="container index"> 
-<div class="accordion">    
+<div class="container index">
+<div class="accordion">
 
-      
-<h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>DDaT Together - October 2018</span></h3> 
+
+<h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>DDaT Together - October 2018</span></h3>
 <div>
 <div class="wrapper">
-   
+
     <p>The next DDaT Together event is scheduled for October 2018. Further details will be added in due course.</p>
-    
-    
+
+
     </div> <!-- wrapper-->
-    
+
   </div><!-- end agenda-->
-    
-    
+
+
     <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Roundtables</span></h3>
   <div>
 
          <div class="wrapper">
             <div class="box white">
-          
-   
+
+
    <p>The Digital, Data and Technology roundtables will take place across our main locations on a quarterly basis. These are the next upcoming proposed DDaT Roundtable dates:</p>
     <ul>
         <li>Bedfont Lakes - 27 March</li>
         <li>Croydon - April &amp; May</li>
         <li>Hendon  - 16 May</li>
-    </ul>           
+    </ul>
 </p>
 
-                
+
         <div class="button_container_first">
                 <a class="btTxt" href="http://ddat-together-roundtable.herokuapp.com">View roundtable events</a>
              </div>
-                
+
 
            </div><!-- end boxwhite-->
-        </div> <!-- wrapper-->    
-      
+        </div> <!-- wrapper-->
+
     </div>
-    
+
   <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT all-staff dial-in event - 16 May 2018</span></h3>
   <div>
 
          <div class="wrapper">
             <div class="box white">
-          
+
    <p>The next all-staff dial-in event is scheduled for 16 May 2018. Further details will be added in due course.</p>
-             
-   
+
+
                   <!-- <div class="button_container_first">
                 <a class="btTxt" href="https://www.eventbrite.co.uk/e/ddat-all-staff-dial-in-tickets-40187871004">Book a ticket</a>
             </div> -->
-                
+
 
            </div><!-- boxwhite-->
-        </div> <!-- wrapper-->    
-      
-      
-      
+        </div> <!-- wrapper-->
+
+
+
     </div>
-    
-    
+
+
     <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Community Events - Coming Soon</span></h3>
   <div>
 
          <div class="wrapper">
             <div class="box white">
-          
+
    <p>Details coming soon.</p>
-             
-   
+
+
                   <!-- <div class="button_container_first">
                 <a class="btTxt" href="https://www.eventbrite.co.uk/e/ddat-all-staff-dial-in-tickets-40187871004">Book a ticket</a>
             </div> -->
-                
+
 
            </div><!-- boxwhite-->
-        </div> <!-- wrapper-->    
-      
-      
-      
-    </div>
-    
-    
-    
-            
-     </div> <!--end CONTAINER--> 
+        </div> <!-- wrapper-->
 
-  
-</div> <!--end ACCORDION--> 
-    
-          
+
+
+    </div>
+
+
+
+
+     </div> <!--end CONTAINER-->
+
+
+</div> <!--end ACCORDION-->
+
+
 
 <?php include('footer.php')?>
