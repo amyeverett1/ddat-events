53f27434c8d6e4835aa69841b63b3cc6faccd06f
Michael <michael.owen6@homeoffice.gsi.gov.uk>
2018-07-12 10:15:47 +0100

    content: latest content update

--- index.php
+++ index.php
@@ -1,87 +1,63 @@
 <?php include('header.php') ?>
+
 <div class="head_box">
-
   <h1>DDaT Together Events</h1>
-
 </div>
-
 
 </header>
 
-
-
 <div class="container index">
-<div class="accordion">
+  <div class="accordion">
 
-
-<h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>DDaT Together - October 2018</span></h3>
-<div>
-<div class="wrapper">
-
-    <p>The next DDaT Together event is scheduled for October 2018. Further details will be added in due course.</p>
-
-
-    </div> <!-- wrapper-->
-
-  </div><!-- end agenda-->
-
+    <h3><i class="fa fa-list-ol fa-lg" aria-hidden="true"></i> <span>DDaT Together - 24 October 2018</span></h3>
+    <div>
+      <div class="wrapper">
+        <p>The next DDaT Together event is scheduled for 24 October 2018. Further details will be added in due course.</p>
+      </div> <!-- wrapper-->
+    </div><!-- end agenda-->
 
     <h3><i class="fa fa-comments fa-lg" aria-hidden="true"></i> <span>DDaT Roundtables</span></h3>
-  <div>
-
-         <div class="wrapper">
-            <div class="box white">
-
-
-   <p>The Digital, Data and Technology roundtables are opportunities for senior leaders to meet with colleagues in DDaT for informal two-way discussions. These events take place across our main locations on a quarterly basis. The next DDaT Roundtable will be held in Hendon on 14 June and will be hosted by Emma Dickson (Deputy Director – Operations, Enterprise Services, Soapworks, Salford).</p>
-   <p>Please contact <a href="mailto:Dawn.Spooner@homeoffice.pnn.police.uk">Dawn.Spooner@homeoffice.pnn.police.uk</a> or <a href="mailto:Rachelle.Hembury@homeoffice.gsi.gov.uk">Rachelle.Hembury@homeoffice.gsi.gov.uk</a> for more information or to book a place.</p>
-
-    
-           </div><!-- end boxwhite-->
-        </div> <!-- wrapper-->
-
+    <div>
+      <div class="wrapper">
+        <div class="box white">
+          <p>The Digital, Data and Technology roundtables are opportunities for senior leaders to meet with colleagues in DDaT for informal two-way discussions. These events take place across our main locations on a quarterly basis.</p>
+          <p>Details of the next DDaT roundtables will be added soon.</p>
+        </div><!-- end boxwhite-->
+      </div> <!-- wrapper-->
     </div>
 
-  <h3><i class="fa fa-volume-control-phone fa-lg" aria-hidden="true"></i> <span>DDaT all-staff dial-in event - 22 May 2018</span></h3>
-  <div>
-
-         <div class="wrapper">
-            <div class="box white">
-
-   <p>The last DDaT staff dial-in was hosted by Joanna Davinson on 22 May. If you have any follow-up questions please use <a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/">this anonymous link</a>.</p>
-
-           </div><!-- boxwhite-->
-        </div> <!-- wrapper-->
-
-
-
+    <h3><i class="fa fa-volume-control-phone fa-lg" aria-hidden="true"></i> <span>DDaT all-staff dial-in event - 23 July 2018</span></h3>
+    <div>
+      <div class="wrapper">
+        <div class="box white">
+          <p>The last DDaT staff dial-in was hosted by Joanna Davinson on 22 May. If you have any follow-up questions please use <a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/">this anonymous link</a>.</p>
+        </div><!-- boxwhite-->
+      </div> <!-- wrapper-->
     </div>
-
 
     <h3><i class="fa fa-users fa-lg" aria-hidden="true"></i> <span>DDaT Champions events and updates</span></h3>
-  <div>
+    <div>
+      <div class="wrapper">
+        <div class="box white">
+          <p>Please read and contribute to the recent blog by Glyn Williams (Chair of the HO Generational Diversity Steering Group): <a href="https://horizon.fcos.gsi.gov.uk/blog/embracing-generational-diversity">horizon.fcos.gsi.gov.uk/blog/embracing-generational-diversity</a>.</p>
+          <p>The DDaT Champions are:</p>
+          <ul>
+            <li>Bristi Gogoi – Gender</li>
+            <li>Rachelle Hembury – Generational Diversity</li>
+            <li>Zuz Kopecka – Sexual Orientation and Gender Identity</li>
+            <li>Sam Layne – Disability</li>
+            <li>Jessica Loudon – Diversity and Social Mobility</li>
+            <li>Jugjit Matharu – Faith</li>
+            <li>Vacant - Race</li>
+          </ul>          
+        </div>
+      </div><!-- boxwhite-->
+    </div> <!-- wrapper-->
 
-         <div class="wrapper">
-            <div class="box white">
-                <p>The DDaT Generational Diversity Champion (Rachelle Hembury) organised a cross-government “All Ages Matter” event with the Age Lead in MoJ (Jodie Keens) to encourage people to be more age aware.  Please look out for follow up articles in the DDaT Insider and follow @XGovAgeGroup on Twitter to keep up-to-date with cross-government developments.</p>
-            </div>
-
-
-           </div><!-- boxwhite-->
-        </div> <!-- wrapper-->
-
-
-
-    </div>
-
-
-
-
-     </div> <!--end CONTAINER-->
+  </div>
+</div> <!--end CONTAINER-->
 
 <p class="browser">For the best experience using this site, we recommend you use Google Chrome as your browser.</p>
 </div> <!--end ACCORDION-->
-
-
 
 <?php include('footer.php')?>
