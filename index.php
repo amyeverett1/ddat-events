4c0c81e2c0cfef6c5b0ae5354912f07a5a4583df
Michael Owen <owenm6@users.noreply.github.com>
2018-09-11 13:49:57 +0100

    With missing links

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
         <div class="box white">
           <p>The Digital, Data and Technology roundtables are opportunities for senior leaders to meet with colleagues in DDaT for informal two-way discussions. These events take place across our main locations on a quarterly basis.</p>
           <p>Booking is now open for the first DDaT roundtable event following the summer break. This session will be held in Southern House in Croydon and co-hosted by Matt Philpot, Director Enterprise Services Technology, and Andy Gregory, Deputy Director Data Services and Analytics, from 14:00-15:30 hours on 27 September.</p>
+          <p><a href="https://www.eventbrite.co.uk/e/ddat-croydon-roundtable-279-tickets-49981072734?utm_term=eventname_text">Book now</a></p>
         </div><!-- end boxwhite-->
       </div> <!-- wrapper-->
     </div>
@@ -30,7 +31,7 @@
     <div>
       <div class="wrapper">
         <div class="box white">
-          <p>The next DDaT all-staff dial-in will be held later in the year. In the meantime, if you have any questions you would like to raise with Joanna and the leadership team please use this anonymous link.</p>
+          <p>The next DDaT all-staff dial-in will be held later in the year. In the meantime, if you have any questions you would like to raise with Joanna and the leadership team please use this <a href="https://www.homeofficesurveys.homeoffice.gov.uk/s/DDaTquestions/">anonymous form</a>.</p>
         </div><!-- boxwhite-->
       </div> <!-- wrapper-->
     </div>
